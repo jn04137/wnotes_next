b6ea8d32f8279346196368b01820a3decb662a94
Jonathan Nguyen <jnganguyen3@gmail.com>
2024-09-12 20:53:27 -0500

    started working on the notes components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,9 @@
+interface Note {
+	title: string,
+	content: string
+}
+
+
 export default async function Home() {
 	console.log(process.env.NEXT_PUBLIC_URL);
 
@@ -7,7 +13,18 @@ export default async function Home() {
 	return(
 		<div>
 			This is the home page
-			{ JSON.stringify(notes) }
+			{ notes.listOfNotes.map((note: Note) => ( <NoteCard note={note} />)) }
+		</div>
+	)
+
+
+}
+
+function NoteCard({note}: {note: Note}) {
+	return (
+		<div>
+			<h1>{note.title}</h1>
+			<div dangerouslySetInnerHTML={{__html: note.content}} />
 		</div>
 	)
 }
